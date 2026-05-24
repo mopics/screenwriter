075c74efc7bd38b157e2af121fe7fec483c31a8e
Peter Dijkstra <pedi@carapaxit.nl>
2026-05-24 09:58:41 +0200

    added relations table

---
+++ src/components/panels/AiChatPanel.tsx
@@ -0,0 +1,17 @@
+export function AiChatPanel() {
+  return (
+    <div className="flex-1 flex items-center justify-center">
+      <div className="text-[#555] text-sm font-mono bg-[#f5f5f5] px-3 py-2 rounded border border-[#ddd]">
+        <pre>
+          Ollama or OpenAI API<br></br>
+          ↓<br></br>
+          Python backend<br></br>
+          ↓<br></br>
+          Character/Astrology engine<br></br>
+          ↓<br></br>
+          Simple chat UI
+        </pre>
+      </div>
+    </div>
+  )
+}
